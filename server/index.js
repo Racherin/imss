fbbde3c817713c11d14bb20ddbe0707a8d62400c
Mert <meertavincan@gmail.com>
2021-04-30 19:46:38 +0300

    cssli version

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
