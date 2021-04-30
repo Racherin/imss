0587d733f43b37e04e7306a6b67ea91c87b9491f
Mert <meertavincan@gmail.com>
2021-04-30 19:16:35 +0300

    remove react

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 // server/index.js
 
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 
-app.get("/api",(req,res) => {
-    res.json({"message":"Hello from server!"});
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.listen(PORT, () => {
